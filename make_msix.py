aeac567befa3d579bc253c320d5a94a472743f37
jangirsachin9619-cpu <jangirsachin9619@gmail.com>
2026-09-26 14:42:02 +0530

    Update make_msix.py

--- make_msix.py
+++ make_msix.py
@@ -1,34 +1,72 @@
-import os
-import shutil
-from pathlib import Path
-from PIL import Image
+import os, shutil, subprocess, glob
 
-# cleanup old folder
-if os.path.exists("build_msix"):
-    shutil.rmtree("build_msix")
+# clean old
+for f in glob.glob("*.msix"):
+    os.remove(f)
+if os.path.exists("msix_build"):
+    shutil.rmtree("msix_build")
 
-os.makedirs("build_msix/Assets", exist_ok=True)
+os.makedirs("msix_build", exist_ok=True)
 
-# 1. copy exe from pyinstaller
-shutil.copy("dist/INSITEVA.exe", "build_msix/INSITEVA.exe")
+# copy exe from dist
+exe_path = glob.glob("dist/*.exe")
+if not exe_path:
+    exe_path = glob.glob("**/INSITEVA.exe", recursive=True)
+if not exe_path:
+    raise Exception("EXE not found in dist/")
 
-# 2. create 3 logos - blue square
-Image.new('RGBA', (150,150), (0,120,212,255)).save("build_msix/Assets/Square150x150Logo.png")
-Image.new('RGBA', (44,44), (0,120,212,255)).save("build_msix/Assets/Square44x44Logo.png")
-Image.new('RGBA', (50,50), (0,120,212,255)).save("build_msix/Assets/StoreLogo.png")
+shutil.copy(exe_path[0], "msix_build/INSITEVA.exe")
 
-# 3. AppxManifest
-Path("build_msix/AppxManifest.xml").write_text('''<?xml version="1.0" encoding="utf-8"?>
-<Package xmlns="http://schemas.microsoft.com/appx/manifest/foundation/windows10" xmlns:uap="http://schemas.microsoft.com/appx/manifest/uap/windows10" xmlns:rescap="http://schemas.microsoft.com/appx/manifest/foundation/windows10/restrictedcapabilities">
-  <Identity Name="Insiteva.INSITEVA" Publisher="CN=4D02E3C1-2496-4104-A71A-89481814EBE0" Version="1.0.12.0" ProcessorArchitecture="x64"/>
-  <Properties><DisplayName>INSITEVA</DisplayName><PublisherDisplayName>Insiteva</PublisherDisplayName><Logo>Assets\\StoreLogo.png</Logo></Properties>
-  <Dependencies><TargetDeviceFamily Name="Windows.Desktop" MinVersion="10.0.17763.0" MaxVersionTested="10.0.22621.0" /></Dependencies>
+# copy assets if exists
+if os.path.exists("assets"):
+    shutil.copytree("assets", "msix_build/assets", dirs_exist_ok=True)
+
+# create AppxManifest.xml
+manifest = """<?xml version="1.0" encoding="utf-8"?>
+<Package xmlns="http://schemas.microsoft.com/appx/manifest/foundation/windows10"
+         xmlns:uap="http://schemas.microsoft.com/appx/manifest/uap/windows10"
+         xmlns:uap10="http://schemas.microsoft.com/appx/manifest/uap/windows10/10">
+  <Identity Name="INSITEVA.App" Publisher="CN=INSITEVA" Version="1.0.12.0" />
+  <Properties>
+    <DisplayName>INSITEVA</DisplayName>
+    <PublisherDisplayName>INSITEVA</PublisherDisplayName>
+    <Logo>assets\\StoreLogo.png</Logo>
+  </Properties>
+  <Dependencies>
+    <TargetDeviceFamily Name="Windows.Desktop" MinVersion="10.0.17763.0" MaxVersionTested="10.0.26100.0" />
+  </Dependencies>
   <Resources><Resource Language="en-us" /></Resources>
-  <Applications><Application Id="App" Executable="INSITEVA.exe" EntryPoint="Windows.FullTrustApplication"><uap:VisualElements DisplayName="INSITEVA" Description="INSITEVA" BackgroundColor="transparent" Square150x150Logo="Assets\\Square150x150Logo.png" Square44x44Logo="Assets\\Square44x44Logo.png" /></Application></Applications>
-  <Capabilities><rescap:Capability Name="runFullTrust" /></Capabilities>
-</Package>''', encoding='utf-8')
+  <Applications>
+    <Application Id="INSITEVA" Executable="INSITEVA.exe" EntryPoint="Windows.FullTrustApplication">
+      <uap:VisualElements DisplayName="INSITEVA" Description="INSITEVA Desktop"
+        BackgroundColor="transparent" Square150x150Logo="assets\\Square150x150Logo.png"
+        Square44x44Logo="assets\\SmallLogo.png" />
+    </Application>
+  </Applications>
+  <Capabilities><Capability Name="runFullTrust" /></Capabilities>
+</Package>
+"""
+with open("msix_build/AppxManifest.xml", "w", encoding="utf-8") as f:
+    f.write(manifest)
 
-# 4. Content Types
-Path("build_msix/[Content_Types].xml").write_text('<?xml version="1.0" encoding="utf-8"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="exe" ContentType="application/vnd.microsoft.portable-exe" /><Default Extension="png" ContentType="image/png" /><Default Extension="xml" ContentType="application/xml" /></Types>', encoding='utf-8')
+# create dummy images if missing
+from PIL import Image
+os.makedirs("msix_build/assets", exist_ok=True)
+for name, size in [("StoreLogo.png",(50,50)), ("Square150x150Logo.png",(150,150)), ("SmallLogo.png",(44,44)), ("Square44x44Logo.png",(44,44))]:
+    p = f"msix_build/assets/{name}"
+    if not os.path.exists(p):
+        Image.new('RGBA', size, (0,120,255,255)).save(p)
 
-print("folder ready")
+# make msix
+# find makeappx
+makeappx = r"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\makeappx.exe"
+if not os.path.exists(makeappx):
+    # try find any version
+    import pathlib
+    kits = list(pathlib.Path(r"C:\Program Files (x86)\Windows Kits\10\bin").rglob("makeappx.exe"))
+    if kits:
+        makeappx = str(kits[0])
+
+print(f"Using {makeappx}")
+subprocess.check_call([makeappx, "pack", "/d", "msix_build", "/p", "INSITEVA_1.0.12.0_x64.msix", "/o"])
+print("MSIX created!")
